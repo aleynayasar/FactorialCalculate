8b0a27d59130c8f5f30515697e989fbeabceacb6
aleynayasar <79570388+aleynayasar@users.noreply.github.com>
2022-03-08 17:09:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RecursiveMetot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter the number you want to calculate the factorial : ");
+            number = int.Parse(Console.ReadLine());
+
+            int factorial = FactorialCalculate(number);
+            Console.WriteLine("Factorial result : {0}",factorial);
+            Console.ReadLine();
+
+        }
+
+        private static int FactorialCalculate(int number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * FactorialCalculate(number - 1);
+
+        }
+        
+    }
+}
